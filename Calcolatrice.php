d0600a7cfdd17248c0217f272a97b3ac5088cf0f
Nicholas <95367571+NichoZ02@users.noreply.github.com>
2022-09-22 12:57:58 +0200

    Add files via upload

---
+++ Calcolatrice.php
@@ -0,0 +1,44 @@
+<html>
+   <body style = 'background-color: lightgrey; margin: 20px'>
+         <h1> Calcolatrice </h1>
+
+         <form name='F1' method='post'>
+            <p> A: <input type = 'text' name = 'T1' size = '5'> B: <input type = 'text' name = 'T2' size = '5'> </p>
+            <input style = "font-size: 20px; margin: 10px" type = 'submit' name = 'B1' value = '+'> 
+            <input style = "font-size: 20px; margin: 10px" type = 'submit' name = 'B2' value = '-'>
+            <input style = "font-size: 20px; margin: 10px" type = 'submit' name = 'B3' value = '*'> 
+            <input style = "font-size: 20px; margin: 10px" type = 'submit' name = 'B4' value = '/'>
+         </form>
+
+         <?php
+            if(isset($_POST['B1']))
+            {
+               $a = $_POST['T1'];
+               $b = $_POST['T2'];
+               $c = $a + $b;
+               echo "Risultato:"." ".$c;
+            }
+            if(isset($_POST['B2']))
+            {
+               $a = $_POST['T1'];
+               $b = $_POST['T2'];
+               $c = $a - $b;
+               echo "Risultato:"." ".$c;
+            }
+            if(isset($_POST['B3']))
+            {
+               $a = $_POST['T1'];
+               $b = $_POST['T2'];
+               $c = $a * $b;
+               echo "Risultato:"." ".$c;
+            }
+            if(isset($_POST['B4']))
+            {
+               $a = $_POST['T1'];
+               $b = $_POST['T2'];
+               $c = $a / $b;
+               echo "Risultato:"." ".$c;
+            }
+         ?>
+      </body>
+</html>
