cf42b21a5e1cb4735700661801eab65cec6e3dd3
Nicholas <95367571+NichoZ02@users.noreply.github.com>
2022-10-11 09:40:14 +0200

    Add files via upload

---
+++ Bottoni_Scomparsa/index.php
@@ -0,0 +1,32 @@
+<?php
+    $r = 9;
+    $c = 9;
+?>
+
+<html>
+    <head>
+        <link rel = "stylesheet" href = "style.css">
+    </head>
+    <body>
+        <form name = 'F1' method = 'POST' action = 'index.php'>
+            <?php
+                echo "<table border = '1'>";
+                    for($j = 0; $j < $r; $j++)
+                    {
+                        echo "<tr>"; 
+                        for($i = 0; $i < $c; $i++)
+                        {
+                            echo "<td>"."<input name = 'B' type = 'submit' value = ''"."</td>"; 
+
+                        }		  
+                        echo "</tr>"; 
+                    }		  
+                echo "</table>";
+            ?>
+
+            <br>
+
+            <input type = 'submit' name = 'R' value = 'Reset'/>
+        </form>
+    </body>
+</html>
