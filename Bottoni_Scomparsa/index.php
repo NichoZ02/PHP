1bbfe9ba4dbc5e66acd0aee0ef8183d0f8631e0d
Nicholas <95367571+NichoZ02@users.noreply.github.com>
2022-10-18 07:27:39 +0200

    Add files via upload

--- Bottoni_Scomparsa/index.php
+++ Bottoni_Scomparsa/index.php
@@ -1,32 +1,57 @@
 <?php
     $r = 5;
     $c = 5;
+
+    $valori = Array();
+    for($i = 0; $i < $r; $i++) 
+    {
+        for($j = 0; $j < $c; $j++) 
+        {
+            $valori[$i][$j] = 0;
+            if(isset($_POST['T'][$i][$j]) && !isset($_POST['reset'])) 
+            {
+                $valori[$i][$j] = 1;
+
+            }
+        }
+    }
 ?>
 
 <html>
     <head>
         <link rel = "stylesheet" href = "style.css">
     </head>
-    <body>
-        <form name = 'F1' method = 'POST' action = 'index.php'>
+    <body align = 'center'>
+        <form name = 'F1' method = 'post' action = 'index.php'>
             <?php
-                echo "<table border = '1'>";
-                    for($j = 0; $j < $r; $j++)
+                echo "<table align = 'center' border = '2';
+                >";
+                    for($i = 0; $i < $r; $i++) 
                     {
                         echo "<tr>"; 
-                        for($i = 0; $i < $c; $i++)
-                        {
-                            echo "<td>"."<input name = 'B' type = 'submit' value = ''"."</td>"; 
+                            for($j = 0; $j < $c; $j++) 
+                            {
+                                echo "<td>";
+                                    if ($valori[$i][$j] == 0) 
+                                    {
+                                        echo "<input type = 'submit' name = 'T[".$i."][".$j."]' value = '' />";
 
-                        }		  
+                                    } 
+                                    else 
+                                    {
+                                        echo "<input type = 'hidden' name = 'T[".$i."][".$j."]' value = '' />";
+
+                                    }
+                                echo "</td>"; 
+                            }	
                         echo "</tr>"; 
-                    }		  
+                    }
                 echo "</table>";
             ?>
 
             <br>
 
-            <input type = 'submit' name = 'R' value = 'Reset'/>
+            <input type = 'submit' name = 'reset'  value = 'Reset'/>
         </form>
     </body>
 </html>
